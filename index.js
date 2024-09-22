af19c8566994eb4dee53932dc8e299ec96117fbd
NightFeather <leolee50910@gmail.com>
2024-09-22 17:45:43 +0800

    fix: script now work with normal videos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              Better YouTube Theater Mode
 // @namespace         https://github.com/NightFeather0615
-// @version           1.0.0
+// @version           1.1.0
 // @description       Make YouTube's theater mode work like Twitch's one
 // @author            NightFeather
 // @match             *://www.youtube.com/*
@@ -39,8 +39,8 @@ const asyncSleep = async (ms) => {
 
     let mashheadContainer = await waitElement("#masthead-container");
     let pageManager = await waitElement("#page-manager");
-    let liveChat = await waitElement("#chat");
     let videoBleedContainer = await waitElement("#full-bleed-container");
+    let liveChat = document.querySelector("#chat");
 
     const processTheaterMode = async () => {
         await asyncSleep(50);
@@ -52,14 +52,20 @@ const asyncSleep = async (ms) => {
             videoBleedContainer.style.height = "100vh";
             mashheadContainer.hidden = true;
             pageManager.style.marginTop = "0px";
-            liveChat.style.top = "0px";
-            liveChat.style.borderRadius = "0 0 0 0"
+
+            if (liveChat) {
+                liveChat.style.top = "0px";
+                liveChat.style.borderRadius = "0 0 0 0";
+            }
         } else {
             videoBleedContainer.style.maxHeight = "";
             videoBleedContainer.style.height = "";
             mashheadContainer.hidden = false;
             pageManager.style.marginTop = "var(--ytd-masthead-height,var(--ytd-toolbar-height))";
-            liveChat.style.borderRadius = "12px"
+
+            if (liveChat) {
+                liveChat.style.borderRadius = "12px";
+            }
         }
 
         let resizeEvent = window.document.createEvent("UIEvents");
